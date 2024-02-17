971b195cd91962f56ffa280b0a8152afa9ffebde
gaurav909 <gaurav150702@gmail.com>
2024-02-17 23:46:26 +0530

    revision of fs and os module

--- server.js
+++ server.js
@@ -1,32 +1,43 @@
-// // // function add(a,b){
-// // //     return a + b
-// // // }
+// // // // function add(a,b){
+// // // //     return a + b
+// // // // }
 
-// // // var add = (a, b) => {
-// // //   return a + b;
-// // // };
-// // // const result = add(6, 4);
-// // // console.log(result)
-// // (function () {
-// //   console.log("gaurav singh");
-// // })();
+// // // // var add = (a, b) => {
+// // // //   return a + b;
+// // // // };
+// // // // const result = add(6, 4);
+// // // // console.log(result)
+// // // (function () {
+// // //   console.log("gaurav singh");
+// // // })();
 
 
-// function callback(){
-//     console.log('now adding is succesfully');
+// // function callback(){
+// //     console.log('now adding is succesfully');
+// // }
+
+// // const add = function(a,b,callback){
+// //     const result = a + b
+// //     console.log(result)
+// //     callback()
+// // }
+// // add(7,8,callback)
+
+// const add = function(a,b,gaurav){
+//     var result = a + b;
+//     console.log(result);
+//     gaurav()
 // }
 
-// const add = function(a,b,callback){
-//     const result = a + b
-//     console.log(result)
-//     callback()
-// }
-// add(7,8,callback)
+// add(4,5,()=>console.log('Add is complted'))
 
-const add = function(a,b,gaurav){
-    var result = a + b;
-    console.log(result);
-    gaurav()
-}
+var fs = require('fs');
+var os= require('os');
 
-add(4,5,()=>console.log('Add is complted'))
+var user = os.userInfo()
+console.log(user)
+console.log(user.username)
+
+fs.appendFile('greating.txt', 'Hello dosto' + user.username + '!\n',()=>{
+    console.log('File is Created')
+})
