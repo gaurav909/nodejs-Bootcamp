aaaf8db7dbbedf7ced86e1fcd063f64753f1f6f1
gaurav909 <gaurav150702@gmail.com>
2024-02-18 00:13:12 +0530

    Learn  new npm lodash

--- server.js
+++ server.js
@@ -1,43 +1,62 @@
-// // // // function add(a,b){
-// // // //     return a + b
-// // // // }
+// // // // // function add(a,b){
+// // // // //     return a + b
+// // // // // }
 
-// // // // var add = (a, b) => {
-// // // //   return a + b;
-// // // // };
-// // // // const result = add(6, 4);
-// // // // console.log(result)
-// // // (function () {
-// // //   console.log("gaurav singh");
-// // // })();
+// // // // // var add = (a, b) => {
+// // // // //   return a + b;
+// // // // // };
+// // // // // const result = add(6, 4);
+// // // // // console.log(result)
+// // // // (function () {
+// // // //   console.log("gaurav singh");
+// // // // })();
 
 
-// // function callback(){
-// //     console.log('now adding is succesfully');
+// // // function callback(){
+// // //     console.log('now adding is succesfully');
+// // // }
+
+// // // const add = function(a,b,callback){
+// // //     const result = a + b
+// // //     console.log(result)
+// // //     callback()
+// // // }
+// // // add(7,8,callback)
+
+// // const add = function(a,b,gaurav){
+// //     var result = a + b;
+// //     console.log(result);
+// //     gaurav()
 // // }
 
-// // const add = function(a,b,callback){
-// //     const result = a + b
-// //     console.log(result)
-// //     callback()
-// // }
-// // add(7,8,callback)
+// // add(4,5,()=>console.log('Add is complted'))
 
-// const add = function(a,b,gaurav){
-//     var result = a + b;
-//     console.log(result);
-//     gaurav()
-// }
+// var fs = require('fs');
+// var os= require('os');
 
-// add(4,5,()=>console.log('Add is complted'))
+// var user = os.userInfo()
+// console.log(user)
+// console.log(user.username)
 
-var fs = require('fs');
-var os= require('os');
+// fs.appendFile('greating.txt', 'Hello dosto' + user.username + '!\n',()=>{
+//     console.log('File is Created')
+// })
 
-var user = os.userInfo()
-console.log(user)
-console.log(user.username)
+// const note = require('./note.js');
 
-fs.appendFile('greating.txt', 'Hello dosto' + user.username + '!\n',()=>{
-    console.log('File is Created')
-})
+// console.log('server file is  available');
+
+// var age = note.age
+
+// var result = note.addNumber(age+18,10)
+// console.log(age)
+// console.log('resulte is now',+result)
+
+var _= require('lodash')
+
+var data = ["gaurav","gaurav",1,2,3,5,1,2,1,8];
+var filter = _.uniq(data)
+console.log(filter)
+
+
+console.log(_.isString('gaurav'))
